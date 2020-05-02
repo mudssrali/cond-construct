0bebd366d2d35b2f4c8cbba7d677ae5e450454c2
Mudassar045 <mudassaralics007@gmail.com>
2020-05-02 05:47:52 +0500

    wire-up pkg stuff

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+export interface Cond {
+	(pairs: CondItem, options: CondOptions): unknown
+}
+type CondItem = Array<[boolean, unknown | (() => unknown)]>
+type CondOptions = { strict: boolean }
+
+const defaults = {
+	strict: true
+}
+
+const cond: Cond = (pairs, options = defaults) => {
+
+	const match = pairs.find(([predicate]) => {
+		return options.strict ? predicate === true : predicate
+	})
+
+	if (!match) return null
+
+	return typeof match[1] === 'function' ? match[1]() : match[1]
+}
+
+export default cond
